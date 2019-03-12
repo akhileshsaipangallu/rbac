cbdae217889b6cfe1ca18bce73ca57d7d668416b
Akhilesh-at-584800525462 <akhilesh.saipangallu@tarams.com>
2019-03-12 23:45:33 +0530

    updated static files

--- rbac/settings.py
+++ rbac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9q@*@0*u1!r77)#ozz#u)3xx&a9k2zqf1w0@u7zu-e@r8h0q9$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+# STATIC_ROOT = '/home/akhilesh/projects/rbac/rbac/static'
